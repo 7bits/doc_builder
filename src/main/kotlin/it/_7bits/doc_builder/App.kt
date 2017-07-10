0e0ada79ce45df05bea3078bc862fb435b8c2f72
Ilya Siganov <ilya.blan4@gmail.com>
2017-07-10 15:34:02 +0600

    simple doc builder

---
+++ src/main/kotlin/it/_7bits/doc_builder/App.kt
@@ -0,0 +1,24 @@
+package it._7bits.doc_builder
+
+import com.beust.jcommander.JCommander
+
+class App {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val options = Options()
+            val jc = JCommander.newBuilder().addObject(options).build().apply {
+                programName = "Document builder"
+            }
+
+            jc.parse(*args)
+
+            if (options.usage) {
+                jc.usage()
+                return
+            }
+
+            Documentation.build(options.source, options.target)
+        }
+    }
+}
---
+++ src/main/kotlin/it/_7bits/doc_builder/Options.kt
@@ -0,0 +1,38 @@
+package it._7bits.doc_builder
+
+import com.beust.jcommander.IStringConverter
+import com.beust.jcommander.Parameter
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class Options {
+    @Parameter(
+            names = arrayOf("--source", "-s"),
+            description = "Path to the Features location",
+            required = false,
+            converter = PathConverter::class
+    )
+    var source: Path = Paths.get("Features")
+
+    @Parameter(
+            names = arrayOf("--target", "-t"),
+            description = "Path to the target location",
+            required = false,
+            converter = PathConverter::class
+    )
+    var target: Path = Paths.get("doc_build")
+
+    @Parameter(
+            names = arrayOf("--help", "--usage"),
+            description = "Display the help"
+    )
+    var usage: Boolean = false
+}
+
+class PathConverter: IStringConverter<Path?> {
+    override fun convert(value: String?): Path? {
+        if (value == null) return null
+        return Paths.get(value)
+    }
+
+}
