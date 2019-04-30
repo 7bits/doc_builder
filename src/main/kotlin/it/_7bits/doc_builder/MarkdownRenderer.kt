12480e550999686baae31538260ec91385e7d4bd
Alexander Ionov <alexander.ionov@7bits.it>
2019-04-30 09:40:29 +0600

    Added option for rendering tables

--- src/main/kotlin/it/_7bits/doc_builder/MarkdownRenderer.kt
+++ src/main/kotlin/it/_7bits/doc_builder/MarkdownRenderer.kt
@@ -1,12 +1,15 @@
 package it._7bits.doc_builder
 
+import com.vladsch.flexmark.ext.tables.TablesExtension
 import com.vladsch.flexmark.html.HtmlRenderer
 import com.vladsch.flexmark.parser.Parser
+import com.vladsch.flexmark.util.options.MutableDataSet
 import java.io.Reader
 
 class MarkdownRenderer: IRenderer {
-    private val parser = Parser.builder().build()
-    private val renderer = HtmlRenderer.builder().build()
+    private val options = MutableDataSet().set(Parser.EXTENSIONS, listOf(TablesExtension.create()))
+    private val parser = Parser.builder(options).build()
+    private val renderer = HtmlRenderer.builder(options).build()
 
     override fun render(reader: Reader): String {
         val node = parser.parseReader(reader)
